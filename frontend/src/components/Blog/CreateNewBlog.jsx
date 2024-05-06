0ec42db05794118e9de63aba909eae3db84b0828
Alisher <alisherxujanov@gmail.com>
2024-05-06 08:50:14 +0500

    finished creation of new-blog form

--- frontend/src/components/Blog/CreateNewBlog.jsx
+++ frontend/src/components/Blog/CreateNewBlog.jsx
@@ -5,12 +5,25 @@ import { useState } from 'react'
 /* eslint-disable no-unused-vars */
 function CreateNewBlog(props) {
     const [form, setForm] = useState({
-        image: "",
+        image: plusImage,
         title: "",
         description: "",
         date: new Date().toLocaleDateString(),
         author: "Surf Auxion"
     })
+
+    function updateForm(e) {
+        if (e.target.name === "image") {
+            const reader = new FileReader()
+            const file = e.target.files[0]
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+                setForm({...form, image: reader.result})
+            }
+        } else {
+            setForm({ ...form, [e.target.name]: e.target.value })
+        }
+    }
 
     function submit(e) {
         e.preventDefault()
@@ -21,20 +34,28 @@ function CreateNewBlog(props) {
         <div className="new-blog-wrapper">
             <form onSubmit={submit}>
                 <div className="form-control">
-                    <label htmlFor="title">Title</label>
-                    <input id="title" type="text" placeholder="Title" />
+                    <label htmlFor="title">Title {form.title}</label>
+                    <input id="title" type="text" placeholder="Title"
+                        onChange={updateForm}
+                        name="title"
+                    />
                     <small className="help-text">Title of your blog</small>
                     <p className="error"></p>
                 </div>
                 <div className="form-control">
-                    <label htmlFor="description">Description</label>
-                    <textarea id='description' placeholder="Description"></textarea>
+                    <label htmlFor="description">Description {form.description}</label>
+                    <textarea id='description' placeholder="Description"
+                        name="description" onChange={updateForm}
+                    ></textarea>
                     <small className="help-text">Description of your blog</small>
                     <p className="error"></p>
                 </div>
                 <div className="form-control">
+                    <img src={form.image} alt="" width={30} height={30}/>
                     <div className='blog-img-input-container'>
-                        <input id='blog-img-input' type="file" placeholder='Insert your image' />
+                        <input id='blog-img-input' type="file" placeholder='Insert your image'
+                            name="image" onChange={updateForm}
+                        />
                         <img src={plusImage} alt="Add image" />
                         <p>Insert your image here</p>
                     </div>
