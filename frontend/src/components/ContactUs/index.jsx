dab356f01d351e8bee2c9bb4aa88b5fee876b207
Alisher <alisherxujanov@gmail.com>
2024-05-20 08:54:05 +0500

    successfully installed email-form

--- frontend/src/components/ContactUs/index.jsx
+++ frontend/src/components/ContactUs/index.jsx
@@ -1,24 +1,29 @@
 import Heading from '../common/Heading/index'
 import ContactImage from '../../assets/imgs/others/Gro.png'
 import './style.scss'
-import { useRef } from 'react'
+import { useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, EMAIL_USER_ID } from '../../private'
 
 function ContactUs() {
-    const form = useRef() // reference to the form element
+    const [form, setForm] = useState({})
+
+    function handleChange(e) {
+        const key = e.target.name
+        const value = e.target.value
+        setForm({ ...form, [key]: value })
+    }
 
     const submitHandler = (e) => {
         e.preventDefault() // prevents the page from reloading when you hit “Send”
-
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID-')
-            .then((result) => {
-                alert('Message Sent', result.text)
-                // show the user a success message
-            }, (error) => {
-                alert('An error occurred, Please try again', error.text)
-                // show the user an error
-            })
-
+        emailjs.send(EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, form, { publicKey: EMAIL_USER_ID })
+            .then(
+                (response) => {
+                    console.log('SUCCESS!', response.status, response.text);
+                },
+                (err) => {
+                    console.log('FAILED...', err);
+                })
         e.target.reset() // resets the form after submission
     }
 
@@ -28,17 +33,16 @@ function ContactUs() {
                 <Heading title={"Contact Us"} page={"Contact page"} />
                 <div className="contact-us">
                     <form className='contact-items'
-                        ref={form}
                         onSubmit={submitHandler}
                     >
                         <h2>Get In Touch</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nihil soluta quasi sequi totam quo sapiente illum vitae debitis et.</p>
                         <div className="tinp">
-                            <input placeholder='Your Name' type="text" />
-                            <input type="text" placeholder='Your E-mail' required />
+                            <input onChange={handleChange} placeholder='Your Name' type="text" name='from_name' required />
+                            <input onChange={handleChange} type="email" placeholder='Your E-mail' name="from_email" required />
                         </div>
-                        <input type="text" placeholder='Subject' required />
-                        <textarea placeholder='Type your message' required></textarea>
+                        <input onChange={handleChange} type="text" placeholder='Subject' name="subject" required />
+                        <textarea onChange={handleChange} placeholder='Type your message' name="message" required></textarea>
                         <button type='submit'>Send Mail</button>
                     </form>
                     <img src={ContactImage} alt="Contact form image" />
---
+++ frontend/src/private.js
@@ -0,0 +1,9 @@
+const EMAIL_SERVICE_ID = "service_ezdkco7"
+const EMAIL_TEMPLATE_ID = "template_dbx4qji"
+const EMAIL_USER_ID = "mbcCG18ZiPltCRfB-"
+
+export {
+    EMAIL_SERVICE_ID,
+    EMAIL_TEMPLATE_ID,
+    EMAIL_USER_ID
+}
